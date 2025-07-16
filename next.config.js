4284098026669bd402b95cf1f590d8d5a8b4a619
Milk Cyber Creatives <milkcybercreatives@gmail.com>
2025-07-16 20:52:54 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   reactStrictMode: true,
+=======
+  output: 'export',
+  images: {
+    unoptimized: true, // ✅ Disables server-side optimization for static export
+  },
+>>>>>>> 7e1e9c6 (Initial commit)
 };
 
 module.exports = nextConfig;
