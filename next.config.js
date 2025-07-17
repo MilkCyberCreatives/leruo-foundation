f6f90cc7ba2418e051e5e625bfe84950cf68d8cf
MilkCyberCreatives <milkcybercreatives@gmail.com>
2025-07-17 13:23:56 +0200

    Fixed header

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   reactStrictMode: true,
-=======
-  output: 'export',
-  images: {
-    unoptimized: true, // ✅ Disables server-side optimization for static export
-  },
->>>>>>> 7e1e9c6 (Initial commit)
-=======
-  reactStrictMode: true,
->>>>>>> dff57c2 (Changed Export)
 };
 
 module.exports = nextConfig;
