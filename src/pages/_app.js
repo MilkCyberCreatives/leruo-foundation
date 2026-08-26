9192e599548718db3e1464ae777049fc5e85a9ec
Milk Cyber Creatives <milkcybercreatives@gmail.com>
2026-08-27 01:52:55 +0200

    Remove unused global Font Awesome stylesheet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import '../styles/custom.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useRouter } from 'next/router';
 import SEO from '@/components/SEO';
 
