763307ef1e6f9238764fdc3aadc291420d34f4d4
Milk Cyber Creatives <milkcybercreatives@gmail.com>
2026-08-27 01:52:44 +0200

    Fix favicon and browser app metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,15 @@ export default function Document() {
   return (
     <Html lang="en-ZA">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/images/leruofoundationicon.png" type="image/png" />
+        <link rel="icon" href="/images/leruofoundationicon.png" type="image/png" sizes="any" />
         <link rel="apple-touch-icon" href="/images/leruofoundationicon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="preconnect" href="https://res.cloudinary.com" />
+        <link rel="dns-prefetch" href="//res.cloudinary.com" />
         <meta name="theme-color" content="#48101f" />
+        <meta name="application-name" content="Leruo Foundation" />
+        <meta name="apple-mobile-web-app-title" content="Leruo Foundation" />
+        <meta name="format-detection" content="telephone=no" />
       </Head>
       <body>
         <Main />
